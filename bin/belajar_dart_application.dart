27f620475bc5be7705e5c73079223064874c921f
Doni Darmawan <doni.duaasattuu@gmail.com>
2024-03-29 19:44:28 +0700

    belajar dart application

--- bin/belajar_dart_application.dart
+++ bin/belajar_dart_application.dart
@@ -1,5 +1,19 @@
-import 'package:belajar_dart_application/belajar_dart_application.dart' as belajar_dart_application;
+// import 'package:belajar_dart_application/belajar_dart_application.dart' as belajar_dart_application;
 
-void main(List<String> arguments) {
-  print('Hello world: ${belajar_dart_application.calculate()}!');
+// void main(List<String> arguments) {
+//   print('Hello world: ${belajar_dart_application.calculate()}!');
+// }
+
+import 'package:belajar_dart_library/hello.dart' as belajar;
+
+void main(List<String> args) {
+  print(belajar.sayHello('Eko'));
+
+  var customer = belajar.Customer();
+  var category = belajar.Category();
+  var product = belajar.Product();
+
+  print(customer);
+  print(category);
+  print(product);
 }
